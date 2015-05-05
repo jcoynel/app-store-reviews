bc91ad974b62fba8cf3ae8fe2ea77840ec481da3
Jules Coynel <julescoynel@gmail.com>
2015-05-05 22:30:46 +0100

    Bump version to 0.0.4

--- examples/reviews-to-mysql.sql
+++ examples/reviews-to-mysql.sql
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014 Jules Coynel */
+/* Copyright (c) 2013-2015 Jules Coynel */
 /* https://github.com/jcoynel/app-store-reviews */
 
 -- --------------------------------------------------------
